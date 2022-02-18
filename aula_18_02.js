4e8bf53e41b0fa3077c9626906da6dee27470c46
Happardo <walissonfelizardo08@gmail.com>
2022-02-18 20:11:16 -0300

    criação de codigos para seleção de frutas cítricas

---
+++ aula_18_02.js
@@ -0,0 +1,22 @@
+const array = [
+    {
+        nome: "banana" ,
+        eCitrica: false, 
+    },
+    {
+        nome: "abacate" ,
+        eCitrica: false, 
+    },  
+    {
+        nome: "limão" ,
+        eCitrica: true, 
+    },      
+    {
+        nome: "laranja" ,
+        eCitrica: true, 
+    },    
+    {
+        nome: "manga" ,
+        eCitrica: false, 
+    }    
+];
